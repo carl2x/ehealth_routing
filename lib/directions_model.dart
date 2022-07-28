ef0cd7e2307d13966a092125606ecea5250ed345
Carl Guo <carlguoshanghai@gmail.com>
2022-07-28 02:56:51 -0700

    Added ignore statement
    Ignored warning for the lack of null-safety

--- lib/directions_model.dart
+++ lib/directions_model.dart
@@ -1,3 +1,4 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
